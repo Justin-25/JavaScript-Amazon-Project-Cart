3e2df5a6a90117ad922a68f06a0176b55319335a
JustinDev <justinmandia25@gmail.com>
2026-04-07 16:19:01 +0800

    Use inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -33,13 +33,18 @@ class Product {
 
   getStarUrl() {
     return `images/ratings/rating-${this.rating.stars * 10}.png`;
-  }
+  };
 
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
+  };
+
+  extraInfoHTML() {
+    return '';
   }
 }
 
+// Class - Practice Code
 const product1 = new Product({
   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -56,8 +61,46 @@ const product1 = new Product({
   ]
 });
 
+// inheritance
+class Clothing extends Product{
+  sizeChartLink
 
-console.log(product1);
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  // Override / replace the parents method
+  // Method overriding
+  extraInfoHTML() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size chart
+      </a>
+    `
+  }
+}
+
+// inheritance - Practice code
+const tshirt = new Clothing({
+  id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+  name: "Adults Plain Cotton T-Shirt - 2 Pack",
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  priceCents: 799,
+  keywords: [
+    "tshirts",
+    "apparel",
+    "mens"
+  ],
+  type: "clothing",
+  sizeChartLink: "images/clothing-size-chart.png"
+});
+
+console.log(tshirt)
 
 export const products = [
   {
@@ -749,5 +792,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  };
   return new Product(productDetails);
 });
